46a5d6ed5a892a06a54385957a55ab6c445ef338
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2024-11-17 01:28:22 -0700

    Update and rename background-container.jsx to background-container.tsx

---
+++ src/components/background/background-container.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const BackgroundContainer: React.FC = () => {
+  return (
+    <div className="background-container">
+      <img
+        src="https://example.com/background.jpg"
+        alt="Background"
+        className="background-image"
+      />
+    </div>
+  );
+}
+
+export default BackgroundContainer;
